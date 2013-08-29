66f15469212d71691718b5ba1dd503e7f37a6cf4
Ausir <lorenzo.fabris@gmail.com>
2013-08-29 14:16:02 +0200

    Added checks for validity of ownerships during commits. Changed date fields for better validation of dates and simplicity of use

--- src/main/java/it/unipd/dei/db/kayak/league_manager/AddOwnershipView.java
+++ src/main/java/it/unipd/dei/db/kayak/league_manager/AddOwnershipView.java
@@ -2,9 +2,13 @@ package it.unipd.dei.db.kayak.league_manager;
 
 import it.unipd.dei.db.kayak.league_manager.data.LMUserDetails;
 import it.unipd.dei.db.kayak.league_manager.data.Ownership;
+import it.unipd.dei.db.kayak.league_manager.data.OwnershipResult;
 import it.unipd.dei.db.kayak.league_manager.database.DML;
 
 import java.sql.Date;
+import java.util.List;
+
+import org.apache.commons.jexl2.UnifiedJEXL.Exception;
 
 import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.ui.Alignment;
@@ -12,6 +16,7 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.CheckBox;
+import com.vaadin.ui.DateField;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
@@ -20,48 +25,36 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 public class AddOwnershipView {
-	private VerticalLayout mainlLayout;
+	private VerticalLayout mainLayout;
 
 	public AddOwnershipView() {
 		this.setUpContent();
 	}
 
 	private void setUpContent() {
-		mainlLayout = new VerticalLayout();
-		mainlLayout.setMargin(new MarginInfo(true, true, true, true));
+		mainLayout = new VerticalLayout();
+		mainLayout.setMargin(new MarginInfo(true, true, true, true));
 
-		mainlLayout.addComponent(new Label("Aggiungi contratto al database"));
+		mainLayout.addComponent(new Label("Aggiungi contratto al database"));
 
-		mainlLayout.addComponent(new Label("ID della società"));
+		mainLayout.addComponent(new Label("ID della società"));
 		final TextField clubIdField = new TextField();
-		mainlLayout.addComponent(clubIdField);
+		mainLayout.addComponent(clubIdField);
 
-		mainlLayout.addComponent(new Label("ID del giocatore"));
+		mainLayout.addComponent(new Label("ID del giocatore"));
 		final TextField playerIdField = new TextField();
-		mainlLayout.addComponent(playerIdField);
+		mainLayout.addComponent(playerIdField);
 
-		mainlLayout.addComponent(new Label("Data di inizio contratto"));
-		HorizontalLayout startDateLayout = new HorizontalLayout();
-		final TextField startDayField = new TextField("Giorno");
-		startDateLayout.addComponent(startDayField);
-		final TextField startMonthField = new TextField("Mese");
-		startDateLayout.addComponent(startMonthField);
-		final TextField startYearField = new TextField("Anno");
-		startDateLayout.addComponent(startYearField);
-		mainlLayout.addComponent(startDateLayout);
+		final DateField startDateField = new DateField("Inizio del contratto");
+		startDateField.setWidth("60px");
+		mainLayout.addComponent(startDateField);
 
-		mainlLayout.addComponent(new Label("Data di fine contratto"));
-		HorizontalLayout endDateLayout = new HorizontalLayout();
-		final TextField endDayField = new TextField("Giorno");
-		endDateLayout.addComponent(endDayField);
-		final TextField endMonthField = new TextField("Mese");
-		endDateLayout.addComponent(endMonthField);
-		final TextField endYearField = new TextField("Anno");
-		endDateLayout.addComponent(endYearField);
-		mainlLayout.addComponent(endDateLayout);
+		final DateField endDateField = new DateField("Inizio del contratto");
+		endDateField.setWidth("60px");
+		mainLayout.addComponent(endDateField);
 
 		final CheckBox borrowedCheckBox = new CheckBox("In prestito", false);
-		mainlLayout.addComponent(borrowedCheckBox);
+		mainLayout.addComponent(borrowedCheckBox);
 
 		HorizontalLayout commitLayout = new HorizontalLayout();
 		Button commitButton = new Button("Inserisci", new ClickListener() {
@@ -71,50 +64,72 @@ public class AddOwnershipView {
 				LMUserDetails user = home.getLoggedInUser();
 				if (user == null || !user.isSectretary()) {
 					Notification
-							.show("Current user doesn't have the necessary privileges");
+							.show("L'utente corrente non ha i privilegi necessari");
 				}
 
 				Ownership ownership = null;
+				MyVaadinUI ui = (MyVaadinUI) UI.getCurrent();
 				try {
+
 					long clubID = Long.parseLong(clubIdField.getValue());
 					long playerID = Long.parseLong(playerIdField.getValue());
 					boolean borrowed = borrowedCheckBox.getValue();
 
-					int day = Integer.parseInt(startDayField.getValue());
-					int month = Integer.parseInt(startMonthField.getValue()) - 1;
-					int year = Integer.parseInt(startYearField.getValue()) - 1900;
-					Date startDate = new Date(year, month, day);
+					java.util.Date rawDate = startDateField.getValue();
+					Date startDate = new Date(rawDate.getTime());
 
-					day = Integer.parseInt(startDayField.getValue());
-					month = Integer.parseInt(startMonthField.getValue()) - 1;
-					year = Integer.parseInt(startYearField.getValue()) - 1900;
-					Date endDate = new Date(year, month, day);
+					rawDate = startDateField.getValue();
+					Date endDate = new Date(rawDate.getTime());
+
+					if (startDate.before(endDate)) {
+						Notification
+								.show("La data di inizio contratto non precede la data fine contratto");
+						return;
+					}
 
 					ownership = new Ownership(0, playerID, clubID, borrowed,
 							startDate, endDate);
+
+					if (!borrowed) {
+						List<OwnershipResult> playerOwnerships = DML
+								.retrieveOwnershipsFromPlayer(
+										ui.getConnection(), playerID);
+						for (OwnershipResult or : playerOwnerships) {
+							if ((startDate.compareTo(or.getStart()) <= 0 && endDate
+									.compareTo(or.getStart()) >= 0)
+									|| (startDate.compareTo(or.getEnd()) <= 0 && endDate
+											.compareTo(or.getEnd()) >= 0)
+									|| (startDate.compareTo(or.getStart()) >= 0 && endDate
+											.compareTo(or.getEnd()) <= 0)) {
+								Notification
+										.show("Il contratto da inserire si sovrappone ad un contratto già registrato");
+								return;
+							}
+						}
+					}
 				} catch (Exception e) {
-					Notification.show("Error in input values");
+					Notification.show("Dati di input non validi");
 				}
 
-				MyVaadinUI ui=(MyVaadinUI) UI.getCurrent();
 				if (!DML.addOwnership(ui.getConnection(), ownership)) {
-					Notification.show("Error during commit");
+					Notification.show("Errore nell'inserimento");
 				} else {
-					Notification.show("Ownership correctly added");
+					Notification.show("Contratto inserito correttamente");
 				}
 			}
 		});
+
 		commitLayout.addComponent(commitButton);
 		commitLayout.setComponentAlignment(commitButton, Alignment.TOP_CENTER);
-		mainlLayout.addComponent(commitLayout);
+		mainLayout.addComponent(commitLayout);
 
 		Label spacer = new Label();
-		mainlLayout.addComponent(spacer);
-		mainlLayout.setExpandRatio(spacer, 1);
+		mainLayout.addComponent(spacer);
+		mainLayout.setExpandRatio(spacer, 1);
 	}
 
 	public VerticalLayout getContent() {
-		return mainlLayout;
+		return mainLayout;
 	}
 
 }
--- src/main/java/it/unipd/dei/db/kayak/league_manager/AddPlayerView.java
+++ src/main/java/it/unipd/dei/db/kayak/league_manager/AddPlayerView.java
@@ -1,16 +1,19 @@
 package it.unipd.dei.db.kayak.league_manager;
 
+import java.sql.Date;
+
 import it.unipd.dei.db.kayak.league_manager.data.LMUserDetails;
 import it.unipd.dei.db.kayak.league_manager.data.Player;
 import it.unipd.dei.db.kayak.league_manager.database.DML;
 
-import java.sql.Date;
+import org.apache.commons.jexl2.UnifiedJEXL.Exception;
 
 import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.DateField;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
@@ -19,35 +22,29 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 public class AddPlayerView {
-	private VerticalLayout mainlLayout;
+	private VerticalLayout mainLayout;
 
 	public AddPlayerView() {
 		this.setUpContent();
 	}
 
 	private void setUpContent() {
-		mainlLayout = new VerticalLayout();
-		mainlLayout.setMargin(new MarginInfo(true, true, true, true));
+		mainLayout = new VerticalLayout();
+		mainLayout.setMargin(new MarginInfo(true, true, true, true));
 
-		mainlLayout.addComponent(new Label("Aggiungi giocatore al database"));
+		mainLayout.addComponent(new Label("Aggiungi giocatore al database"));
 
-		mainlLayout.addComponent(new Label("ID"));
+		mainLayout.addComponent(new Label("ID"));
 		final TextField idField = new TextField();
-		mainlLayout.addComponent(idField);
+		mainLayout.addComponent(idField);
 
-		mainlLayout.addComponent(new Label("Nome"));
+		mainLayout.addComponent(new Label("Nome"));
 		final TextField nameField = new TextField();
-		mainlLayout.addComponent(nameField);
+		mainLayout.addComponent(nameField);
 
-		mainlLayout.addComponent(new Label("Data di nascita"));
-		HorizontalLayout dateLayout = new HorizontalLayout();
-		final TextField dayField = new TextField("Giorno");
-		dateLayout.addComponent(dayField);
-		final TextField monthField = new TextField("Mese");
-		dateLayout.addComponent(monthField);
-		final TextField yearField = new TextField("Anno");
-		dateLayout.addComponent(yearField);
-		mainlLayout.addComponent(dateLayout);
+		final DateField birthdayField = new DateField("Giorno di nascita");
+		birthdayField.setWidth("60px");
+		mainLayout.addComponent(birthdayField);
 
 		HorizontalLayout commitLayout = new HorizontalLayout();
 		Button commitButton = new Button("Inserisci", new ClickListener() {
@@ -57,7 +54,7 @@ public class AddPlayerView {
 				LMUserDetails user = home.getLoggedInUser();
 				if (user == null || !user.isSectretary()) {
 					Notification
-							.show("Current user doesn't have the necessary privileges");
+							.show("L'utente corrente non ha i privilegi necessari");
 				}
 
 				Player player = null;
@@ -65,34 +62,33 @@ public class AddPlayerView {
 					long id = Long.parseLong(idField.getValue());
 					String name = nameField.getValue();
 
-					int day = Integer.parseInt(dayField.getValue());
-					int month = Integer.parseInt(monthField.getValue()) - 1;
-					int year = Integer.parseInt(yearField.getValue()) - 1900;
-					Date birthday = new Date(year, month, day);
+					java.util.Date rawDate = birthdayField.getValue();
+					Date birthday = new Date(rawDate.getTime());
 
 					player = new Player(id, name, birthday);
 				} catch (Exception e) {
-					Notification.show("Error in input values");
+					Notification.show("Dati di input non validi");
 				}
 
-				MyVaadinUI ui=(MyVaadinUI) UI.getCurrent();
+				MyVaadinUI ui = (MyVaadinUI) UI.getCurrent();
 				if (!DML.addPlayer(ui.getConnection(), player)) {
-					Notification.show("Error during commit");
+					Notification.show("Errore nell'inserimento");
 				} else {
-					Notification.show("Player correctly added");
+					Notification.show("Giocatore inserito correttamente");
 				}
 			}
 		});
+		
 		commitLayout.addComponent(commitButton);
 		commitLayout.setComponentAlignment(commitButton, Alignment.TOP_CENTER);
-		mainlLayout.addComponent(commitLayout);
+		mainLayout.addComponent(commitLayout);
 
 		Label spacer = new Label();
-		mainlLayout.addComponent(spacer);
-		mainlLayout.setExpandRatio(spacer, 1);
+		mainLayout.addComponent(spacer);
+		mainLayout.setExpandRatio(spacer, 1);
 	}
 
 	public VerticalLayout getContent() {
-		return mainlLayout;
+		return mainLayout;
 	}
 }
